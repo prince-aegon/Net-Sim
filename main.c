4e75d7c6c88f75d1f5d576c2bde4e2c3ff41212d
sarthak jha <sarthak.jha@arista.com>
2023-01-25 23:10:49 +0530

    Grid created and cells can be marked

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ const int screenHeight = 450;
 const int vertical_sep = 50;
 const int horizontal_sep = 75;
 
+const int lineGap = 10;
 typedef struct
 {
     int pointX;
@@ -14,28 +15,32 @@ typedef struct
 
 void createGrid()
 {
-    Point startv, endv, starth, endh;
     Point points[4];
-    for (int i = horizontal_sep; i < screenWidth - horizontal_sep; i += 10)
+    for (int i = horizontal_sep; i < screenWidth - horizontal_sep; i += lineGap)
     {
         points[0].pointX = i;
         points[0].pointY = vertical_sep;
 
         points[1].pointX = i;
-        points[1].pointY = screenHeight - vertical_sep - 10;
+        points[1].pointY = screenHeight - vertical_sep - lineGap;
 
         DrawLine(points[0].pointX, points[0].pointY, points[1].pointX, points[1].pointY, LIGHTGRAY);
     }
-    for (int i = vertical_sep; i < screenHeight - vertical_sep; i += 10)
+    for (int i = vertical_sep; i < screenHeight - vertical_sep; i += lineGap)
     {
         points[2].pointX = horizontal_sep;
         points[2].pointY = i;
 
-        points[3].pointX = screenWidth - horizontal_sep - 10;
+        points[3].pointX = screenWidth - horizontal_sep - lineGap;
         points[3].pointY = i;
 
         DrawLine(points[2].pointX, points[2].pointY, points[3].pointX, points[3].pointY, LIGHTGRAY);
     }
+}
+
+void mark(Point mPoint)
+{
+    DrawRectangle(horizontal_sep + lineGap * mPoint.pointX, vertical_sep + lineGap * mPoint.pointY, lineGap, lineGap, WHITE);
 }
 
 int main(void)
@@ -52,7 +57,12 @@ int main(void)
 
         createGrid();
 
-        // mark(int x, int y);
+        Point mPoint;
+        mPoint.pointY = 1;
+        for (mPoint.pointX = 0; mPoint.pointX < (screenWidth - horizontal_sep * 2 - lineGap) / lineGap; mPoint.pointX += 2)
+        {
+            mark(mPoint);
+        }
 
         EndDrawing();
     }
